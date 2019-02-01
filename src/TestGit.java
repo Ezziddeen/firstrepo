8aa72f1736147872b231ac32649a65ee01500d62
Ezziddeen AbdulWahhab <ezoo_111@hotmail.com>
2019-02-01 18:32:41 +0200

    This is my second commit with doc included

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,6 +1,20 @@
 
 public class TestGit {
 
+	/**
+	   * This method is used to add two integers. This is
+	   * a the simplest form of a class method, just to
+	   * show the usage of various javadoc Tags.
+	   * @param x This is the first paramter to myFunction method
+	   * @param y This is the second parameter to myFunction method
+	   * @return int This returns sum of x and y.
+	   */
+	public static int myFunction(int x, int y) {
+		
+		
+		return x+y;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
